b6c2731e8de6a5ba71acb82c416ae6b368faffda
kontogithubszkola <julian.stajnke02@gmail.com>
2023-04-05 23:35:53 +0200

    Initial

---
+++ app/src/main/java/com/example/duzezadanieocena/MainActivity.kt
@@ -0,0 +1,105 @@
+package com.example.duzezadanieocena
+
+import android.content.Intent
+import android.os.Bundle
+import android.transition.TransitionInflater
+import android.view.View
+import android.view.animation.AnimationUtils
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityOptionsCompat
+import com.google.android.material.button.MaterialButtonToggleGroup
+import com.google.android.material.navigation.NavigationView
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        var logged = false
+        var User_Data = arrayOf("", "", "", "")
+
+        val data = intent.getStringArrayExtra("userinfo")
+        if (data != null) {
+            User_Data = data
+            RefreshUserUIElements(User_Data)
+            logged = true
+            findViewById<LinearLayout>(R.id.Login_Form).visibility = View.GONE
+            findViewById<TextView>(R.id.Textview_form_text).visibility = View.VISIBLE
+        }
+
+        supportActionBar?.hide()
+
+        // Obsługa chowania się manu nawigacji oraz jego animowanie
+        findViewById<Button>(R.id.Button_nav_close).setOnClickListener {
+            val navigationView = findViewById<NavigationView>(R.id.nav_view)
+            val animation = AnimationUtils.loadAnimation(this, R.anim.slide_out_left)
+            navigationView.startAnimation(animation)
+            navigationView.visibility = View.GONE
+        }
+
+        // Obsługa pokazywania się manu nawigacji oraz jego animowanie
+        findViewById<ImageView>(R.id.button_hamburger_menu).setOnClickListener {
+            val navigationView = findViewById<NavigationView>(R.id.nav_view)
+            val animation = AnimationUtils.loadAnimation(this, R.anim.slide_in_left)
+            navigationView.startAnimation(animation)
+            navigationView.visibility = View.VISIBLE
+        }
+
+        // Obsługa przejś
+        findViewById<Button>(R.id.Button_nav_homepage).setOnClickListener {
+            Toast.makeText(this, "Jesteś już na stronie głównej", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.Button_nav_marks).setOnClickListener {
+            if (logged){
+                startActivity(Intent(this, tabela_ocen::class.java).putExtra("userinfo", User_Data))
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_in_left)
+            }else
+                Toast.makeText(this, "Zaloguj się!", Toast.LENGTH_SHORT).show()
+        }
+
+        findViewById<Button>(R.id.Button_nav_user_info).setOnClickListener {
+            if (logged){
+                startActivity(Intent(this, Informacje_o_uzytkowniku::class.java).putExtra("userinfo", User_Data))
+                overridePendingTransition(R.anim.slide_in_left, R.anim.slide_in_left)
+            }else
+                Toast.makeText(this, "Zaloguj się!", Toast.LENGTH_SHORT).show()
+
+        }
+
+        // Obsługa przycisku logowania
+        findViewById<Button>(R.id.Button_login).setOnClickListener {
+            val nick = findViewById<EditText>(R.id.Input_nick).text.toString()
+            val imie = findViewById<EditText>(R.id.Input_imie).text.toString()
+            val nazwisko = findViewById<EditText>(R.id.Input_nazwisko).text.toString()
+            val klasa = findViewById<EditText>(R.id.Input_klasa).text.toString()
+
+            if (nick != "" && imie != "" && nazwisko != "" && klasa != ""){
+                User_Data[0] = nick
+                User_Data[1] = imie
+                User_Data[2] = nazwisko
+                User_Data[3] = klasa
+
+                logged = true
+
+                findViewById<LinearLayout>(R.id.Login_Form).visibility = View.GONE
+                findViewById<TextView>(R.id.Textview_form_text).visibility = View.VISIBLE
+                RefreshUserUIElements(User_Data)
+            }else{
+                Toast.makeText(this, "Wypełnij wszystkie pola!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    fun RefreshUserUIElements(User_Data: Array<String>):Unit{
+        findViewById<TextView>(R.id.TextView_nav_username).text = User_Data[0]
+        findViewById<TextView>(R.id.Textview_Toolbar_text).text = "${User_Data[1]} ${User_Data[2]}"
+    }
+}
